9c9ab9ff6dc9ad6b33d0c6b958a300c9f09becbe
Jonathan Haylett <jonathan@haylett.dev>
2024-01-09 12:16:41 +0000

    improve following code

--- src/common/follow.ts
+++ src/common/follow.ts
@@ -1,8 +1,9 @@
 import { Entity } from "typed-adventureland";
-import { StateKey, getState, setState } from "../state";
+import { AttackMode, StateKey, getState, setState } from "../state";
 import { debug_log } from "../util";
 
-const FOLLOW_DISTANCE = 60;
+const COMBAT_FOLLOW_DISTANCE = 200;
+const FOLLOW_DISTANCE = 80;
 
 function startFollowing(entity: Entity) {
     if (entity == null) {
@@ -30,31 +31,43 @@ async function followTask() {
     if (followingId == null) {
         return;
     }
-    const following = parent.entities[followingId];
+    const following =
+        parent.entities[followingId] ??
+        get_characters().find((character) => character.name === followingId);
     if (following == null) {
+        debug_log(`followingId ${followingId} not found`);
         return;
     }
 
+    const attackMode = getState(StateKey.ATTACK_MODE);
+    const followDistance =
+        attackMode === AttackMode.FARMING_ACTIVE ? COMBAT_FOLLOW_DISTANCE : FOLLOW_DISTANCE;
     const distanceFromFollowee = simple_distance(
-        { x: character.x, y: character.y },
-        { x: following.x, y: following.y },
+        { x: character.x, y: character.y, map: character.map },
+        { x: following.x, y: following.y, map: following.map },
     );
-    if (distanceFromFollowee > FOLLOW_DISTANCE) {
+    // debug_log(
+    //     `character.x: ${character.x}, character.y: ${character.y}, character.map: ${character.map}, character.real_x: ${character.real_x}, character.real_y: ${character.real_y}`,
+    // );
+    // debug_log(
+    //     `following.x: ${following.x}, following.y: ${following.y}, following.map: ${following.map}, following.real_x: ${following.real_x}, following.real_y: ${following.real_y}`,
+    // );
+    if (distanceFromFollowee > followDistance || following.map !== character.map) {
         debug_log(`distanceFromFollowee: ${distanceFromFollowee}`);
         debug_log(`xmoving to ${following.x}, ${following.y}`);
-        if (can_move_to(following.x, following.y)) {
+        if (following.map === character.map && can_move_to(following.x, following.y)) {
+            debug_log(`move(${following.x}, ${following.y})`);
             await move(following.x, following.y);
         } else {
-            await smart_move({ x: following.x, y: following.y, map: following.map });
+            debug_log(
+                `smart_move({real_x: ${following.real_x}, real_y: ${following.real_y}, map: ${following.map}})`,
+            );
+            await smart_move({
+                x: following.x,
+                y: following.y,
+                map: following.map,
+            });
         }
-        // await xmove(following.x, following.y);
-        // await xmove({
-        //     x: following.x,
-        //     y: following.y,
-        //     map: following.map,
-        //     real_x: following.real_x,
-        //     real_y: following.real_y,
-        // });
     }
 }
 
